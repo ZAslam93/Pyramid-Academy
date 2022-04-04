a64dff60fe7510f9a7143dec6b8f68b00b857302
Zubair Aslam <zubul93@yahoo.com>
2022-04-04 11:40:48 -0500

    Spring commits

---
+++ empSpringBoot/src/main/java/com/genspark/empSpringBoot/EmpSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.empSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSpringBootApplication.class, args);
+	}
+
+}
---
+++ empSpringBoot/src/test/java/com/genspark/empSpringBoot/EmpSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.empSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
